5edc1c0cddc5696d9f8786366edae5cd56ff6ce5
Carlos Junior <cjuniorfox@gmail.com>
2024-09-27 18:59:37 -0300

    Podman updates (#3)
    * possibilidade de novas configurações DHCP
    
    * ubound fix permissions
    
    * dhcp server creation with leases file
    
    * Podman support
    
    * adjustaments for making work with my setup
    
    * fix unbound conf
    
    * doing pod.yaml
    
    * conflict resolver
    
    * Updated the dockerfile and published a new github workflow
    
    * run every sunday night
    
    * update dockerfile
    
    * updated the github action
    
    * Revert "updated the github action"
    
    This reverts commit e694f64e744ef6009e86b703ddef00b8e3884768.
    
    * chore: added podman version
    
    * configured as correct
    
    * Files organization
    
    * Added a README.md and updated the Dockerfile creation
    
    * merge from develop
    
    * updated the github action
    
    * Revert "updated the github action"
    
    This reverts commit e694f64e744ef6009e86b703ddef00b8e3884768.
    
    * fixed the version capturing
    
    * Update docker-publish.yml
    
    * Update docker-publish.yml
    
    * Update docker-publish.yml
    
    * Update docker-publish.yml
    
    * Update docker-publish.yml
    
    * removed old pod
    
    * fixed kubernetes path and network.sh
    
    ---------
    
    Co-authored-by: Junior_FOX <junior@macmini.juniorfox.net>

--- network.sh
+++ network.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-LAN=br0
-GUEST=vlan3
+ZONE='internal'
 IP_POD="$(cat kubernetes/pod.yaml | grep ip: | awk '{print $2}')"
-FORWARD="forward-port=port=53:proto=udp:toport=53:toaddr=${IP_POD} --zone=internal"
+FORWARD="forward-port=port=53:proto=udp:toport=53:toaddr=${IP_POD} --zone=${ZONE}"
 
 #The firewall-cmd forward rule forwards any request targeting the gateway the podman container, don't matter what host is. If you prefer just sharing the port with the network. Just comment out the firewall-cmd rules below
 echo -e "Creating Firewall rule with the following command:\n  firewall-cmd --add-${FORWARD}"
