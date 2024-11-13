632c98d5117397731b0b4b57a026dc87fa52812f
Junior <cjuniorfox@gmail.com>
2024-11-13 14:35:24 -0300

    removing volumes and leases file

--- unbound/usr/local/sbin/unbound_watcher.sh
+++ unbound/usr/local/sbin/unbound_watcher.sh
@@ -10,6 +10,11 @@ elif [ "${DHCPSERVER}" == "kea" ]; then
 	WATCHER="/usr/local/sbin/unbound_kea_watcher.py"
 fi
 
+if [ ! -f '/dhcp.leases' ]; then
+	echo "Leases file /dhcp.leases not found. Exiting..."
+	exit 1
+fi
+
 python3 ${WATCHER} \
 	--foreground --source /dhcp.leases \
 	--target /etc/unbound/unbound.conf.d/dhcpleases.conf \
