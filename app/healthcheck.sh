3d63657b4a35ce00cd4b12a4f40a66f392b322e0
Junior_FOX <cjuniorfox@gmail.com>
2025-08-08 08:27:55 -0300

    fix: healthcheck error

--- app/healthcheck.sh
+++ app/healthcheck.sh
@@ -1,6 +1,4 @@
-#/bin/ash
+#!/bin/ash
 PORT=$1
-if [[ -z "$PORT" ]];
-	PORT=53
-fi
-dig @localhost -p $PORT google.com | grep "status: NOERROR" || exit 1
+PORT="${PORT:-53}"
+dig @localhost -p "$PORT" google.com | grep "status: NOERROR" > /dev/null || exit 1
